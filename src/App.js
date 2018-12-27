00646a2354712eeeae29403c5fba801b2858dac2
Kovalev Stanislav <djently@yandex.ru>
2018-12-27 20:31:09 +0300

    store current section in sessionStorage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { set, get } from 'lodash/fp'
 import React, { Component } from 'react'
 import { CssBaseline, AppBar, Toolbar } from '@material-ui/core'
 
+import { STORAGE_SECTION } from "./constants";
 import config from './sample_config/config'
 import schema from './schema.json'
 import getDefaultPath from './utils/getDefaultPath'
@@ -11,11 +12,11 @@ import ConfigComponent from './components/ConfigComponent'
 
 class App extends Component {
   state = {
-    section: getDefaultPath(schema),
+    section: restoreSection() || getDefaultPath(schema),
     config: config,
   }
 
-  handleSectionChange = (key, section) => this.setState({ section })
+  handleNavigate = section => this.setState({ section }, () => storeSection(section))
   handleConfigChange = path => value =>
     this.setState({ config: set(path, value, this.state.config) })
 
@@ -31,7 +32,7 @@ class App extends Component {
             <ConfigNavigation
               schema={schema}
               value={this.state.section}
-              onNavigate={section => this.setState({ section })}
+              onNavigate={this.handleNavigate}
             />
           </Toolbar>
         </AppBar>
@@ -46,3 +47,15 @@ class App extends Component {
 }
 
 export default App
+
+function storeSection(section) {
+  if (window.sessionStorage) {
+    window.sessionStorage.setItem(STORAGE_SECTION, section)
+  }
+}
+
+function restoreSection() {
+  if (window.sessionStorage) {
+    return window.sessionStorage.getItem(STORAGE_SECTION)
+  }
+}
