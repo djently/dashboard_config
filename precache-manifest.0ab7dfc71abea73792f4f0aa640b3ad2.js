2df7bdc358a747a23525c7e4b7ab203b58bfea88
Kovalev Stanislav <djently@yandex.ru>
2018-12-28 12:28:31 +0300

    Updates

---
+++ precache-manifest.0ab7dfc71abea73792f4f0aa640b3ad2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d53e2f16e40e5f8332e",
+    "url": "/dashboard_config/static/js/runtime~main.9d53e2f1.js"
+  },
+  {
+    "revision": "f2d3a0e1ff2039462f40",
+    "url": "/dashboard_config/static/js/main.f2d3a0e1.chunk.js"
+  },
+  {
+    "revision": "03f60dbeeaa2ae39b63f",
+    "url": "/dashboard_config/static/js/1.03f60dbe.chunk.js"
+  },
+  {
+    "revision": "ecec4a731c0589c0bc5898c90f489257",
+    "url": "/dashboard_config/index.html"
+  }
+];
